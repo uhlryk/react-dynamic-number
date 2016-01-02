e0b7e7f253342e32e6a6157e1166fdbd14007ee4
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-01-02 20:36:32 +0100

    component set className for input; component callback onChange, onModelChange, onViewChange

--- src/dynamicNumber.jsx
+++ src/dynamicNumber.jsx
@@ -2,8 +2,45 @@ import React from 'react';
 
 class DynamicNumber extends React.Component {
 
+  static propTypes = {
+    value: React.PropTypes.number
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modelValue: 0,
+      viewValue: '0'
+    }
+
+    this.onChange= this.onChange.bind(this);
+  }
+
+  onChange(evt) {
+    var rawValue = evt.target.value;
+    var modelValue = parseFloat(rawValue);
+    var viewValue = rawValue;
+
+    if(this.props.onChange) {
+      this.props.onChange(evt, modelValue, viewValue);
+    }
+
+    if(this.props.onModelChange) {
+      this.props.onModelChange(modelValue);
+    }
+
+    if(this.props.onViewChange) {
+      this.props.onViewChange(viewValue);
+    }
+
+    this.setState({
+      modelValue: modelValue,
+      viewValue: viewValue
+    });
+  }
+
   render() {
-    return <input type="text" />
+    return <input type="text" className={this.props.className} value={this.state.viewValue} onChange={this.onChange} />
   }
 }
 
